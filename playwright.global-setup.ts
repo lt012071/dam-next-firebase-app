179754945decf6e31f7394926474a2cc05855087
m-isozaki <m-isozaki@owndays.co.jp>
2025-07-04 02:16:03 +0900

    chore: e2eデバッグ用にブラウザのconsole/pageerrorも出力

--- playwright.global-setup.ts
+++ playwright.global-setup.ts
@@ -9,6 +9,8 @@ if (!process.env.CI) {
 async function globalSetup(config: FullConfig) {
   const browser = await chromium.launch();
   const page = await browser.newPage();
+  page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+  page.on('pageerror', error => console.log('PAGE ERROR:', error));
   await page.goto('http://localhost:3000/login-test');
   console.log(await page.content());
   await page.waitForSelector('input[aria-label="メールアドレス"]', { timeout: 60000 });
