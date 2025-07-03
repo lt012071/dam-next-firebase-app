cbdb5b9f14d0677dc502d1d926972619a3e0b838
m-isozaki <m-isozaki@owndays.co.jp>
2025-07-03 12:23:50 +0900

    リファクタリングと不要ファイル削除

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 import styles from "@/styles/Layout.module.css";
 import Image from "next/image";
 import { useRouter } from "next/router";
